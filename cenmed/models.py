64e226a7370bf07cf2f9e843dc2a0d12bfeb5e95
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-07-30 01:52:34 -0300

    Agregue Models

--- cenmed/models.py
+++ cenmed/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Medico(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    matricula = models.IntegerField()
+    especialidad= models.CharField(max_length=30)
+    email= models.EmailField()
+
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    identificacion= models.IntegerField()
+    email= models.EmailField()
+    habitacion = models.IntegerField()
+
+class Habitacion(models.Model):
+    numero = models.IntegerField()
+    ocupado= models.BooleanField()
+
+class Medicamento(models.Model):
+    nombre= models.CharField(max_length=30)
+    inventario= models.IntegerField()
+    requiere_receta= models.BooleanField()
