9693373ca127cfb1b4cd43016db5999f55c4c4aa
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-07-30 08:39:20 -0300

    Agregue los list y filter view

--- cenmed/admin.py
+++ cenmed/admin.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 
 from .models import *
 
+class MedicoAdmin(admin.ModelAdmin):
+    list_display= ("nombre", "apellido", "matricula", "especialidad", "email")
+    list_filter= ("nombre",)
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display= ("nombre", "apellido", "identificacion", "email", "habitacion")
+    list_filter= ("nombre",)
+
+class MedicamentoAdmin(admin.ModelAdmin):
+    list_display= ("nombre", "inventario", "requiere_receta")
+    list_filter= ("nombre",)
+
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display= ("numero", "ocupado")
+    list_filter= ("nombre",)
+
 # Register your models here.
-admin.site.register(Medico)
-admin.site.register(Paciente)
-admin.site.register(Medicamento)
-admin.site.register(Habitacion)
+admin.site.register(Medico, MedicoAdmin)
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Medicamento, MedicamentoAdmin)
+admin.site.register(Habitacion, HabitacionAdmin)
